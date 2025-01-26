d2915fb674259a7c9a9593a747e4f8d037a0ae0d
Ayuik <marcosayelen@gmail.com>
2025-01-26 17:20:42 +0100

    input, button and list created

--- vite-project/src/ToDoList.jsx
+++ vite-project/src/ToDoList.jsx
@@ -1,11 +1,26 @@
-import React, {useState} from 'react'
+import React, {useState} from 'react';
 
 function ToDoList (){
+    const [tasks, setTasks] = useState([]);
+
+   
+
+    function handleAddTask (){
+        const newTask = document.getElementById('taskInput').value
+        setTasks([...tasks, newTask])
+    }
+
 
     return (
-        <h1>Hola</h1>
+        <>
+            <h1>To do list:</h1>
+            <input type="text" placeholder="Enter a task" id="taskInput"></input>
+            <button onClick={handleAddTask}>Add task to the list</button>
+            <p>
+                {tasks}
+            </p>
+        </>
     )
-
 }
 
 export default ToDoList
